20d51fa0303ae7ca576af39466713354d5cbb906
Raul Sanchez R. <raul.sr92@gmail.com>
2026-09-14 16:54:33 -0500

    Clase 149 Inicio de proyecto - instalacion tailwind, daysiUI, creacion directorios, paginas y archivo de rutas del 1er router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'basic',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page')
+  },
+  {
+    path:'numbers',
+    title: 'Pipes Numéricos',
+    loadComponent: () => import('./pages/numbers-page/numbers-page')
+  },
+  {
+    path:'uncommon',
+    title: 'Pipes No Comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page')
+  },
+  {
+    path:'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page')
+  },
+  {
+    path:'**',
+    redirectTo: 'basic'
+  }
+];
