26e697400f46331d30e9b607e00015f60559b246
Raul Sanchez R. <raul.sr92@gmail.com>
2026-09-21 15:37:30 -0500

    Clase 152 - Decimal Percer y Currency pipes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+
   ]
 };
--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, CurrencyPipe, PercentPipe],
   templateUrl: './numbers-page.html',
 })
-export default class NumbersPage {}
+export default class NumbersPage {
+
+  //Signals
+
+    totalSells = signal(2_020_239.5567)
+
+    percent = signal(0.4856)
+}
